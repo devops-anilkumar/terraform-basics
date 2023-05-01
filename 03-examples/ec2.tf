1c51b35059c26d55c02f1a6d7a9172352f114bfb
unknown <anilkumarravuri510@gmail.com>
2023-05-01 17:50:29 +0530

    auto push

--- 03-examples/ec2.tf
+++ 03-examples/ec2.tf
@@ -5,8 +5,9 @@ provider "aws" {
 }
 
 resource "aws_instance" "web" {
-  ami           = "ami-01a4c5cf14f30168e"
-  instance_type = "t2.micro"
+  ami                     = "ami-01a4c5cf14f30168e"
+  instance_type           = "t2.micro"
+  vpc_security_group_ids  = aws_security_group.allow_ssh_sg.id
 
   tags = {
     Name        = "terraform instance_server_name"
