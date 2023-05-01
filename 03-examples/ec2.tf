48ade6a038a4ab15fb7f31db3a3db339a95f22db
unknown <anilkumarravuri510@gmail.com>
2023-05-01 17:31:35 +0530

    auto push

--- 03-examples/ec2.tf
+++ 03-examples/ec2.tf
@@ -19,8 +19,8 @@ output "private_dns_of_server" {
 }
 
 #CREATES SECURITY GROUP
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
+resource "aws_security_group" "allow_ssh_sg" {
+  name        = "b53_allow_ssh-sg"
   description = "Allow ssh inbound traffic"
 
   ingress {
